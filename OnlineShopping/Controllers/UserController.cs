72e38396004adf2f1ec6c027813d3ffdd865e7b5
hoanvn1999 <hoanvn1999@gmail.com>
2021-01-27 15:55:16 +0700

    Create new user

---
+++ OnlineShopping/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using OnlineShopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShopping.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(USER obj)
+
+        {
+            if (ModelState.IsValid)
+            {
+                OnlineShoppingEntities db = new OnlineShoppingEntities();
+                db.USERS.Add(obj);
+                db.SaveChanges();
+            }
+            return View(obj);
+        }
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        public ActionResult PostSignIn()
+        {
+            return RedirectToRoute("Index", "Home");
+        }
+
+        public ActionResult SignOut()
+        {
+            return RedirectToRoute("Index", "Home");
+        }
+    }
+}
